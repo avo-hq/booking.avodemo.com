bff76eaf593324443f11b7fa46812e80f01d5379
Exequiel Rozas <exequiel98@gmail.com>
2024-12-06 04:29:23 -0400

    Add basic location show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  mount Avo::Engine, at: Avo.configuration.root_path
   devise_for :users
+
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Avo::Engine, at: Avo.configuration.root_path
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,6 +14,7 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  resources :locations, only: [:show]
 
   # Defines the root path route ("/")
   root "home#index"
