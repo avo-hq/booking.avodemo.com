52a89b89bdf2a1215c8bc67fafc164c7789cdf30
Exequiel Rozas <exequiel98@gmail.com>
2024-12-06 04:03:40 -0400

    Remove 'registerable' and 'recoverable' modules to avoid exposing sign up and password recovery routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,8 +23,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :rememberable, :validatable
   extend FriendlyId
   friendly_id :full_name, use: :slugged
 
