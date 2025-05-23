ac61d033a9d4e4017306d0b6fb1c3925399337fe
Exequiel Rozas <exequiel98@gmail.com>
2025-05-22 21:44:14 -0400

    Match 'booked_for' hours from 1 to 8

--- app/avo/resources/booking.rb
+++ app/avo/resources/booking.rb
@@ -13,9 +13,13 @@ class Avo::Resources::Booking < Avo::BaseResource
     field :room, as: :belongs_to
     field :booked_at, as: :date_time
     field :booked_for, as: :select, options: {
+      '1 hour' => 1,
       '2 hours' => 2,
+      '3 hours' => 3,
       '4 hours' => 4,
+      '5 hours' => 5,
       '6 hours' => 6,
+      '7 hours' => 7,
       '8 hours' => 8,
     }
   end
